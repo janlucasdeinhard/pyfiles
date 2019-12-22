c2eadf657190462344595ec4054c6d89aef5f202
Jan-Lucas Deinhard <jan-lucas.deinhard@fau.de>
2019-12-22 23:53:09 +0100

    created a function to randomly assemble pdf/cdf

---
+++ 13_PROJECT_READER/MAIN.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <exception>
+#include <stdarg.h>
+#include <opencv2\opencv.hpp>
+
+#define COLOR_BLACK 0
+#define COLOR_WHITE 255
+
+#define BUFFERSIZE 5
+
+using namespace std;
+using namespace cv;
+
+// Declare functions
+
+// Main function implementation
+int main(){
+	printf("Hello USA");
+	
+	
+	getchar();
+	return 0;
+}
+
+// Implement functions
