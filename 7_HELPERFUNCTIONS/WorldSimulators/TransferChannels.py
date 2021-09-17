a0657a1e38a724066b694c4d6223596745848ccb
Deinhard <jan-lucas.deinhard@siemens-healthineers.com>
2021-09-17 14:58:04 +0200

    Created shared helper function directory

---
+++ 7_HELPERFUNCTIONS/WorldSimulators/TransferChannels.py
@@ -0,0 +1,17 @@
+from numpy import random as rnd
+
+class MIMOChannel:    
+    def __init__(self,n,m,mu_d=0,sigma_d=0):
+        
+        self.out_size = n
+        self.in_size = m
+        self.H = rnd.randn(n,m)
+        self.mu_d = mu_d
+        self.sigma_d = sigma_d
+    def transfer_v(self,x):
+        try:
+            x = x.reshape(self.in_size,-1)
+        except: raise Exception('Wrong input shape. Expected shape (-1,{}), found shape {}'.format(self.in_size,x.shape))
+        n = self.sigma_d*rnd.randn(self.out_size,1) + self.mu_d
+        y = self.H.dot(x) + n
+        return y.reshape(-1,self.out_size)
